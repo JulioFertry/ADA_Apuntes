88e6ee0db468b826e13d834f433520dc1d6b88b0
JulioFertry <juliofertry@gmail.com>
2024-10-03 20:41:18 +0200

    Tema 2 - Transform (Nuevo Producto)

--- src/main/kotlin/apuntesXML/TeoriaEscrituraXML.kt
+++ src/main/kotlin/apuntesXML/TeoriaEscrituraXML.kt
@@ -47,24 +47,43 @@ fun main() {
      *      </productos>
      */
 
-    // a) Primero creamos el Element
-    val producto1: Element = docu.createElement("productos")
+    // a) Primero creamos el Element de producto1
+    val producto1: Element = docu.createElement("producto")
+        // element de producto2
+    val producto2: Element = docu.createElement("producto")
+
+    // Adjuntamos el producto1
     docu.documentElement.appendChild(producto1)
+        // y el producto2
+    docu.documentElement.appendChild(producto2)
 
     // b) Hijos de producto1
     val nombreP1: Element = docu.createElement("nombre")
     val precioP1: Element = docu.createElement("precio")
+        // hijos de producto2
+    val nombreP2: Element = docu.createElement("nombre")
+    val precioP2: Element = docu.createElement("precio")
 
+    // TextNodes de producto1
     val textoNombreP1: Text = docu.createTextNode("Agua")
-    val textoPrecioP1: Text = docu.createTextNode("1.50")
+    val textoPrecioP1: Text = docu.createTextNode("1.19")
+        // textNodes de producto2
+    val textoNombreP2: Text = docu.createTextNode("Galletas")
+    val textoPrecioP2: Text = docu.createTextNode("2.35")
 
     // Unimos el textNode al elemento correspondiente
     nombreP1.appendChild(textoNombreP1)
     precioP1.appendChild(textoPrecioP1)
+        // lo mismo para el otro producto
+    nombreP2.appendChild(textoNombreP2)
+    precioP2.appendChild(textoPrecioP2)
 
     // Unimos el nombre y el precio al producto
     producto1.appendChild(nombreP1)
     producto1.appendChild(precioP1)
+        //lo mismo para el otro producto
+    producto2.appendChild(nombreP2)
+    producto2.appendChild(precioP2)
 
     // Añadimos todos los nodos que queramos
 
