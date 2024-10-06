4027d7cbeb80f434459237b46b34787a9f1a57c1
JulioFertry <juliofertry@gmail.com>
2024-10-06 23:53:32 +0200

    Act 4

--- src/main/kotlin/actividadesXML/act4/Main_Act4.kt
+++ src/main/kotlin/actividadesXML/act4/Main_Act4.kt
@@ -1,8 +1,32 @@
 package actividadesXML.act4
 
+import java.nio.file.Path
+
 
 fun main() {
 
     val operationProcessor = Operations()
+
+    val productsPath = Path.of("src")
+        .resolve("main")
+        .resolve("resources")
+        .resolve("archivosXML")
+        .resolve("productos.xml")
+
+    val productsWithVATFile = Path.of("src")
+        .resolve("main")
+        .resolve("resources")
+        .resolve("archivosXML")
+        .resolve("productsWithVAT.xml")
+        .toFile()
+
+
+    try {
+        val products = Reader.getProdcuts(productsPath)
+        val productsWithVAT = operationProcessor.calculateProductsWithVAT(products)
+        Writer.writeNewProducts(productsWithVATFile, productsWithVAT)
+    } catch (e: Exception) {
+        println("***ERROR*** ${e.message}")
+    }
 
 }
